0ffac1e2f4a34d7d1280bb2db9061a4286a7ff5b
Mir Arshan Ali <109890622+arshan-alee@users.noreply.github.com>
2023-06-15 12:19:37 +0500

    Dotenv.config removed

--- controller/main.controller.js
+++ controller/main.controller.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const stripe = require("stripe")("sk_test_51NHUo0IXBDZqa5GLMa0CdC07H5SPA5EUK8mRurNvlLUEMVncweDYoizTUrtUG9pnr6jiphx6CwsbzPRSp7bssciN00vRgs4a3i");
 
-dotenv.config();
 
 class MainController {
   stripeCheckout = async (req, res, next) => {
